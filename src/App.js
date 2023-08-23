4c84a18f058daba10b5caa6c79c7fbd57e6156a1
VINÍCIUS SOARES FRANÇA <vinicius_s_franca@estudante.sesisenai.org.br>
2023-08-23 17:12:10 -0300

    Início

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Home from './tela-match/Home.jsx';
+import Home from './tela-match/Mprojetos.jsx';
 
 const App = () => {
     return (
         <div>
             <Router>
                 <Routes>
-                    <Route path='/home' element={<Home />} />
+                    <Route path='/Mprojetos' element={<Home />} />
+                    <Route path='/' element={<Cadastro />} />
                 </Routes>
             </Router>
         </div>
