d75215c4acf90c0b57dc731bf8e9bebc36954368
VINÍCIUS SOARES FRANÇA <vinicius_s_franca@estudante.sesisenai.org.br>
2023-08-25 14:36:26 -0300

    Alterações Iniciais

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import TelaMatch from './tela-match/tela.jsx';
-import { ReactComponent as Icon } from './tela-match/logo.svg';
+import { ReactComponent as Icon } from '../public/favicon.ico';
 
 const App = () => {
     return (
