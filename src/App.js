e108d66efe09e7268b60cb6e1ea297deb9c106c2
VINÍCIUS SOARES FRANÇA <vinicius_s_franca@estudante.sesisenai.org.br>
2023-08-28 14:03:32 -0300

    CSS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import TelaMatch from './tela-match/tela.jsx';
-import { ReactComponent as Icon } from '../public/favicon.ico';
 
 const App = () => {
     return (
         <div>
-            <Icon />
             <Router>
                 <Routes>
                     <Route path='/match' element={<TelaMatch />} />
