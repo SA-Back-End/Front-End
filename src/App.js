3f79cdcc9367be182411318f6ef7ba92fc519823
VINÍCIUS SOARES FRANÇA <vinicius_s_franca@estudante.sesisenai.org.br>
2023-08-30 15:33:51 -0300

    CSS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Logo from './logo.ico';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <table>
+          <tr>
+            <td>
+              <div className='one'>
+                <img src={Logo} alt="logo" />
+                <figcaption>S K I L L S</figcaption>
+              </div>
+            </td>
+            <td><a href=''>Quem somos?</a></td>
+            <td><a href=''>Cadastre-se</a></td>
+            <td><a href=''>Login</a></td>
+          </tr>
+        </table>
+
       </header>
+
     </div>
   );
 }
