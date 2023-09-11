864b3fc010264f61b150751b24162d7cc4fffadf
VINÍCIUS SOARES FRANÇA <vinicius_s_franca@estudante.sesisenai.org.br>
2023-09-11 15:46:59 -0300

    CSS

--- src/App.js
+++ src/App.js
@@ -4,26 +4,28 @@ import Logo from './logo.ico';
 
 const App = () => {
   return (
-    <div className='tela'>
-      <nav className='topo'>
-        <ul className='lista'>
-          <li>
-            <img src={Logo} className='logo' alt="logo" />
-            <figcaption className='legenda'>S K I L L S</figcaption>
-          </li>
+    <div>
+      <div className='tela'>
+        <nav className='topo'>
           <ul className='lista'>
-            <li className='topics'><a className='redirect' href=''>Quem somos?</a></li>
-            <li className='topics'><a className='redirect' href=''>Cadastre-se</a></li>
-            <li className='topics'><a className='redirect' href=''>Login</a></li>
+            <li>
+              <img src={Logo} className='logo' alt="logo" />
+              <figcaption className='legenda'>S K I L L S</figcaption>
+            </li>
+            <ul className='lista'>
+              <li className='topics'><a className='redirect' href=''>Quem somos?</a></li>
+              <li className='topics'><a className='redirect' href=''>Cadastre-se</a></li>
+              <li className='topics'><a className='redirect' href=''>Login</a></li>
+            </ul>
           </ul>
-        </ul>
-      </nav>
-      <div className='welcome'>
-        <h1 className='nome'>S K I L L S</h1>
-        <p className='slogan'>Encontre os parceiros <span className='laranja'>ideais</span> para os seus projetos</p>
-        <a href=''>
-          <button className='btn' htype="button">Saiba mais</button>
-        </a>
+        </nav>
+        <div className='welcome'>
+          <h1 className='nome'>S K I L L S</h1>
+          <p className='slogan'>Encontre os parceiros <span className='laranja'>ideais</span> para os seus projetos</p>
+          <a href=''>
+            <button className='btn' htype="button">Saiba mais</button>
+          </a>
+        </div>
       </div>
     </div>
   );
