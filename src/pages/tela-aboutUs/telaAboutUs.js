5b8ebec36480c759730f8e9228f7bac2fce29845
VINÍCIUS SOARES FRANÇA <vinicius_s_franca@estudante.sesisenai.org.br>
2023-09-11 14:12:30 -0300

    CSS

--- src/pages/tela-aboutUs/telaAboutUs.js
+++ src/pages/tela-aboutUs/telaAboutUs.js
@@ -1,46 +1,32 @@
 import React from 'react';
 import './telaAboutUs.css';
-import logo from './img/logo.ico';
+import Logo from './img/logo.ico';
 
-const TelaAboutUs = () => {
-    return (
-        <div>
-            <div className='tela-principal'>
-                <header className='header-aboutUs'>
-
-                    <div class="container">
-                        <img src={logo} alt="logo" class="img-logo" />
-                        <span className='icon-skills'><b>Skills</b></span>
-                    </div>
-
-                    <nav className='nav-links'>
-                        <ul className='links-toOtherPages'>
-                            <li><a href="/" className='options-links'>Quem somos?</a></li>
-                            <li><a href="/cadastro" className='options-links'>Cadastre-se</a></li>
-                            <li><a href="/login" className='options-links'>Login</a></li>
-                        </ul>
-                    </nav>
-
-                </header>
-
-                <div class="div-center">
-                    <h1 className='skills'>Skills</h1>
-                    <p className='merchan'>Encontre os parceiros <span className='span-aboutUs'>ideais</span> para os seus projetos</p>
-                    <button type="button" className='btn-saibaMais' href="#main">Saiba mais</button>
-                </div>
-            </div>
-
-            <div id='main'>
-                <div id='container2'>
-                    <h1>Mais controle, mais interação!</h1>
-                    <p>lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem </p>
-                </div>
-                <div id='imgTemplate'>
-                    <p>IMAGEM</p>
-                </div>
-            </div>
-        </div>
-    );
+const App = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li className='logo'>
+            <img src={Logo} alt="logo" />
+            <figcaption>S K I L L S</figcaption>
+          </li>
+          <ul>
+            <li className='topics'><a href=''>Quem somos?</a></li>
+            <li className='topics'><a href=''>Cadastre-se</a></li>
+            <li className='topics'><a href=''>Login</a></li>
+          </ul>
+        </ul>
+      </nav>
+      <div className='welcome'>
+        <h1>S K I L L S</h1>
+        <p>Encontre os parceiros <span>ideais</span> para os seus projetos</p>
+        <a href=''>
+          <button htype="button">Saiba mais</button>
+        </a>
+      </div>
+    </div>
+  );
 }
 
-export default TelaAboutUs;
+export default App;
