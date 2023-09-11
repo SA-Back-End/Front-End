0af7dd3f6d7d62d8e5232359392038599ccfdf9d
VINÍCIUS SOARES FRANÇA <vinicius_s_franca@estudante.sesisenai.org.br>
2023-09-11 14:43:46 -0300

    CSS

--- src/App.js
+++ src/App.js
@@ -4,25 +4,25 @@ import Logo from './logo.ico';
 
 const App = () => {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li className='logo'>
-            <img src={Logo} alt="logo" />
-            <figcaption>S K I L L S</figcaption>
+    <div className='tela'>
+      <nav className='topo'>
+        <ul className='lista'>
+          <li>
+            <img src={Logo} className='logo' alt="logo" />
+            <figcaption className='legenda'>S K I L L S</figcaption>
           </li>
-          <ul>
-            <li className='topics'><a href=''>Quem somos?</a></li>
-            <li className='topics'><a href=''>Cadastre-se</a></li>
-            <li className='topics'><a href=''>Login</a></li>
+          <ul className='lista'>
+            <li className='topics'><a className='redirect' href=''>Quem somos?</a></li>
+            <li className='topics'><a className='redirect' href=''>Cadastre-se</a></li>
+            <li className='topics'><a className='redirect' href=''>Login</a></li>
           </ul>
         </ul>
       </nav>
       <div className='welcome'>
-        <h1>S K I L L S</h1>
-        <p>Encontre os parceiros <span>ideais</span> para os seus projetos</p>
+        <h1 className='nome'>S K I L L S</h1>
+        <p className='slogan'>Encontre os parceiros <span className='laranja'>ideais</span> para os seus projetos</p>
         <a href=''>
-          <button htype="button">Saiba mais</button>
+          <button className='btn' htype="button">Saiba mais</button>
         </a>
       </div>
     </div>
