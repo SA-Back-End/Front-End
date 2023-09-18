470b20d232c6a6de676a33c466ca7865ed9929f2
Lucasfranken131 <lucas.franco@aluno.fmpsc.edu.br>
2023-09-18 17:00:33 -0300

    Feat: abrir comentário

--- src/ForYou/Feed.jsx
+++ src/ForYou/Feed.jsx
@@ -3,6 +3,7 @@ import './Posts.css';
 import Like from './like.png';
 import Liked from './like_clicked.png';
 import Comment from './comment.png';
+import Arrow from './arrow.png';
 
 const Feed = () => {
 
@@ -13,23 +14,32 @@ const Feed = () => {
     const [page, setPage] = useState(1);
     const [dataLength, setDataLength] = useState()
     const [liked, setLiked] = useState({});
-    const [commented, setCommented] = useState(false)
 
-    /*função 'likado'*/
-
-    const toggleLike = (postId) => {
+    //Define se foi dado o like ou não
+    const toggleLike = (id_post) => {
         setLiked((prevLiked) => ({
             ...prevLiked,
-            [postId]: !prevLiked[postId],
+            [id_post]: !prevLiked[id_post],
         }));
     };
 
+    //Troca a visibilidade da barra de comentário
+    const toggleComment = (postId) => {
+        setItems((prevItems) =>
+            prevItems.map((item) =>
+                item.id_post === postId
+                    ? { ...item, commentVisible: !item.commentVisible }
+                    : item
+            )
+        );
+    };
 
     const fetchData = async () => {
         setError(null);
 
+        //Chamada do Back-End para apresentar os posts na página
         try {
-            const response = await fetch(`http://localhost:3000/user/findAll/${page}`);
+            const response = await fetch(`http://localhost:3000/post/findAll/${page}`);
             const data = await response.json();
             console.log(data);
 
@@ -50,10 +60,12 @@ const Feed = () => {
         }
     }
 
+    //Coloca o FetchData em uso ao abrir ou recarregar a página
     useEffect(() => {
         fetchData();
     }, []);
 
+    //Verifica se o final da tela foi alcançado e faz a chamada do FetchData
     const handleScroll = () => {
         if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight || isLoading) {
             return;
@@ -61,46 +73,60 @@ const Feed = () => {
         fetchData();
     };
 
+    //Verifica se o final da tela foi alcançado e faz a chamada do handleScroll.
     useEffect(() => {
         window.addEventListener('scroll', handleScroll);
         return () => window.removeEventListener('scroll', handleScroll);
     }, [isLoading]);
 
+    //retorna os valores do Back-end como HTML
     return (
         <div>
             {items.map(item => (
-                <React.Fragment key={item.id_user}>
+                <React.Fragment key={item.userId}>
                     <div className="Post">
                         <div className="Post-header">
-                            <div><img src={profilePicture} width="60px" height="60px" alt={item.username} id="profilePicture" /></div>
-                            <div><strong>{item.username}</strong></div>
+                            <div><img src={profilePicture} width="60px" height="60px" alt={item.userId} id="profilePicture" /></div>
+                            <div><strong>{item.user.username}</strong></div>
                         </div>
                         <div className="Post-content">
                             <div>{item.status}</div>
-                            <div>{item.firstName}</div>
-                            <div>{item.lastName}</div>
-                            <div>{item.email}</div>
-                            <div>{item.description}</div>
-                            <div>{item.birthDate}</div>
-                            <div>{item.state}</div>
+                            <div>{item.text}</div>
+                            <div>{item.likedBy}</div>
                         </div>
                         <div className="Post-action">
                             <div>
                                 <button
-                                    className={`action-button like-button ${liked[item.username] ? 'liked' : ''}`}
-                                    onClick={() => toggleLike(item.username)} // Use o id da postagem como argumento
+                                    className={`action-button like-button ${liked[item.id_post] ? 'liked' : ''}`}
+                                    onClick={() => toggleLike(item.id_post)}
                                     id="like"
                                 >
                                     <img
-                                        src={liked[item.username] ? Liked : Like} // Use LikeRed quando liked for verdadeiro, senão, use LikeGray
+                                        src={liked[item.id_post] ? Liked : Like} //Troca a imagem do like quando for dado o like
                                         width="30px"
                                         height="30px"
                                         alt="Curtir"
                                     />
                                 </button>
 
-                                <button id="comment"><img src={Comment} width="30px" height="30px" alt="Comentar" /></button>
+                                <button
+                                    className={`action-button comment-button ${item.id_post}`}
+                                    onClick={() => toggleComment(item.id_post)}
+                                    id="comment-button"
+                                >
+                                    <img 
+                                        src={Comment} 
+                                        width="30px" 
+                                        height="30px" 
+                                        alt="Comentar" />
+                                </button>
                             </div>
+                        </div>
+
+                        <div className="Comment-section">
+                            <div><img src={profilePicture} width="30px" height="30px" alt={item.userId} id="profilePicture" hidden={!item.commentVisible}/></div>
+                            <input id="comment" name="comment" placeholder="Comentário" hidden={!item.commentVisible}/>
+                            <button id="submit" hidden={!item.commentVisible}><img src={Arrow} alt="Comentar" width="25px" height="30px" /></button>
                         </div>
                     </div>
                 </React.Fragment>
