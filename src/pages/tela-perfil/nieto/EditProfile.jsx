246d741601575b20ee0b06b397777dc888344712
LucasSGonza <lucassilva.goncalves@hotmail.com>
2023-09-20 17:17:25 -0300

    feat: telaPerfil + first upt from branch nieto

---
+++ src/pages/tela-perfil/nieto/EditProfile.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './EditProfile.css';
+import HomeNavbar from './components/navbar/navbar';
+const EditProfile = () => {
+    return (
+        <div className='EditProfile'>
+            <header>
+                <HomeNavbar />
+            </header>
+
+            <div className='gradient'>
+
+            </div>
+        </div>
+    );
+}
+
+export default EditProfile;
--- src/pages/tela-perfil/telaPerfil.js
+++ src/pages/tela-perfil/telaPerfil.js
@@ -1,11 +1,62 @@
-import PerfilTay from "./tay/perfil-tay";
+// import PerfilTay from "./tay/perfil-tay";
+import Logo from '../tela-aboutUs/img/logo.ico';
+import iconPerfil from './img/mamaco.jpg';
+import Header from './nieto/components/navbar/navbar.js'
+import "./telaPerfil.css";
 
 function TelaPerfil() {
     return (
         <div>
-            <PerfilTay></PerfilTay>
+            <header>
+                <Header/>
+            </header>
+            <div className='div-perfil'>
+                <div className='container-perfil'>
+                    <div className='container-perfil-infos'>
+                        <img src={iconPerfil} className='iconPerfil' alt='icon' />
+                        <div className='div-infosLeft'>
+                            <p>
+                                <h2>Happy Monke 🍌</h2>
+                                @happyMonke22
+                            </p>
+                            <div className='accountFollows'>
+                                <div className='div-forInfos'>
+                                    <span className='infoFollowing'>236</span>
+                                    <span>Seguindo</span>
+                                </div>
+                                <hr className='lineDivisor'/>
+                                <div className='div-forInfos'>
+                                    <span className='infoFollowers'>433</span>
+                                    <span>Seguidores</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='div-infosRight'>
+                            <ul>
+                                <li>Engenharia/Tecnologia</li>
+                                <li>Disponibilidade limitada</li>
+                                <li>25 de agosto</li>
+                                <li>Santa Catarina</li>
+                            </ul>
+                        </div>
+                        <div className='div-button'>
+                            <button>Editar Perfil</button>
+                        </div>
+                    </div>
+                </div>
+                <div className='container-perfil-aboutMe'>
+                    <div className='container-perfil-aboutMe-infos'>
+                        <h2>Sobre mim</h2>
+                        <p>Olá pessoal! Me chamo Jorginho, tenho 22 anos e sou formado
+                            em Marketing. Atualmente moro em Palhoça/SC e curso sistemas
+                            de informação na UNISUL. Sou muito criativo, divertido e proativo
+                            e busco projetos que me permitam aprender mais!
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
-
+{/* <PerfilTay></PerfilTay> */ }
 export { TelaPerfil }
