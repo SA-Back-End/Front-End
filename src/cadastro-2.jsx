c2063084cf22675de75a79d10113246fce75ea2d
ISADORA ADREA THOLL <isadora_tholl@estudante.sesisenai.org.br>
2023-10-04 16:31:25 -0300

    fodase

--- src/cadastro-2.jsx
+++ src/cadastro-2.jsx
@@ -1,8 +1,8 @@
-import './App.css';
+import './cadastro.css';
 import Data from './Modules/Data/data.js'
 import Logo from './img/logo.js';
 import React, { useState } from 'react';
-import Barrafixa from './barra-fixa/barra_fixa';
+import Barrafixa from './barra_fixa';
 import desenho from './img/desenho.png';
 import logo from './img/logo_skills.png'
 
@@ -26,22 +26,20 @@ function App() {
       <div className='quadrado-branco'>
         <h1>S K I L L S</h1>
         <h2>Bem vindo! Informe os dados abaixo para criar sua conta.</h2>
-        <div className="nome">
-          <input type="text" className="primeiro_nome" placeholder='Primeiro Nome' />
-          <input type="text" className="ultimo_nome" placeholder='Último Nome' /><br />
+        <div className="aleatorio">
+          <input type="text" className="ultimo_nome" placeholder='Unidade Federativa' /><br />
         </div>
-        <div className='user_email'>
-          <input type="text" className="usuario" placeholder='Usuário' /> <br></br>
-          <input type="text" className="email" placeholder='E-mail' />
-        </div>
-        {/* <div className="credenciais">
+        <div className="credenciais">
             <input type="password" className="senha" id="senha" placeholder='Senha' /><br />
             <input type="password" className="confirmar_senha" id="confirmar_senha" placeholder='Confirme a Senha' /><br />
           </div>
-          <input type="text" className="descricao" placeholder='Descrição' /><br /> */}
-        {/* <Data></Data> */}
+          <input type="text" className="descricao" placeholder='Descrição' /><br />
+        <Data></Data>
+        <div className='button_back'>
+          <button className="botao" type="submit" >back</button>
+        </div>
         <div className='button_submit'>
-          <button className="botao" type="submit" >Próximo➜</button>
+          <button className="botao" type="submit" onClick={() => verificarSenhasIguais()}>Criar uma conta</button>
         </div>
         <p className='com_login'>Já tem uma conta? <span className='laranjaa'>Faça login</span></p>
       </div>
