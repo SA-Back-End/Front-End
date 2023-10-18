d09aa14ed8ae0f7a6b2a6076f8592cd5e48e6cef
lestofus2 <rodrigo.rodriguesbrasil07@gmail.com>
2023-10-18 15:05:03 -0300

    addLele

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-//import './App.css';
+import Habilidade from './components/componentesLele/habilidadeL';
 import Skill from './components/skill';
 import Formacao from './components/formacaoModal';
 import { Multiselect } from './components/multiselect';
 
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <div>
+        <h2>Skills</h2>
+        <div class="flex-container">
+          <h3>Hard Skills</h3>
+        <Multiselect/>
+        </div>
+        <div>
+          <h3>Soft Skills</h3>
+        <Multiselect/>
+        </div>
+      </div>  
+      <Habilidade/>
       <Formacao/>
+      
     </div>
-  );
+  )
 }
+
+
+
 
 //<Skill/>
 export default App;
+
