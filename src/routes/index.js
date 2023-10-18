794af080f9628409345c9e9f66752b6411d955ea
LucasSGonza <lucassilva.goncalves@hotmail.com>
2023-10-18 17:14:08 -0300

    wip: need to review telaPerfil and the interation with telaLogin and telaCadastro. Its working but maybe in not the right way

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import TelaCadastro from '../pages/tela-cadastro/telaCadastro.js'
 import TelaMatch from '../pages/tela-match/telaMatch.js';
 import TelaForYou from '../pages/tela-forYou/telaForYou.js';
-import TelaAboutUs from '../pages/tela-aboutUs/telaAboutUs.js';
+import TelaAboutUs from '../pages/Home/telaAboutUs.js';
 import { TelaPerfil } from '../pages/tela-perfil/telaPerfil.js';
 import TelaLogin from '../pages/tela-login/login.js';
-import TelaCarrossel from '../pages/tela-aboutUs/tela-carrossel/telaCarrossel.js';
 import EditProfile from "../pages/tela-perfil/nieto-EditProfile/EditProfile.js"
 import Nieto from '../pages/nieto/nieto.js';
 
@@ -22,7 +21,6 @@ function WebRoutes() {
                         <Route path="/perfil" element={<TelaPerfil />} />
                         <Route path="/login" element={<TelaLogin />} />
                         <Route path="/cadastro" element={<TelaCadastro />} />
-                        <Route path='/carrossel' element={<TelaCarrossel />} />
                         <Route path='/editProfile' element={<EditProfile />} />
                         <Route path='/nieto' element={<Nieto />} />
                     </Routes>
