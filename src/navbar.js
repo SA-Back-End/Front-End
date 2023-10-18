4484caf57dde506abc9601cd07414985f5e91ba8
ISADORA ADREA THOLL <isadora_tholl@estudante.sesisenai.org.br>
2023-10-18 15:59:24 -0300

    action para os icones na navbar

---
+++ src/navbar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './navbar.css';
+import logo from './images/logo.ico'
+import profilePicture from '../../tela-perfil/nieto-EditProfile/profilePicture.png'
+import { BsFillGearFill, BsBellFill, BsSearch } from 'react-icons/bs'
+import { Link } from 'react-router-dom';
+
+const HomeNavbar = () => {
+    return (
+        <div className='HomeNavbar'>
+            <header>
+                <nav>
+                    <ul>
+                        <li className='logoFlex'>
+                            <Link to={"/match"}><img src={logo} alt='logo' id='logo' /></Link>
+                            <p>Skills</p>
+                        </li>
+                        <ul className='icon-buttons'>
+                            <li>
+                                <Link to={"/forYou"}><BsSearch className='icons' /></Link>
+                            </li>
+                            <li>
+                                <BsBellFill className='icons' />
+                            </li>
+                            <li>
+                                <BsFillGearFill className='icons' />
+                            </li>
+                            <li>
+                                <Link to={"/perfil"}><img src={profilePicture} alt='profile' id='profilePicture' /></Link>
+                            </li>
+                        </ul>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+    );
+}
+
+export default HomeNavbar;
