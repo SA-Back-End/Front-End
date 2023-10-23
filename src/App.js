9cd4388467799710d065147ce647a4e69b2fb544
lestofus2 <rodrigo.rodriguesbrasil07@gmail.com>
2023-10-23 16:08:03 -0300

    css

--- src/App.js
+++ src/App.js
@@ -2,24 +2,51 @@ import Habilidade from './components/componentesLele/habilidadeL';
 import Skill from './components/skill';
 import Formacao from './components/formacaoModal';
 import { Multiselect } from './components/multiselect';
+import React, { useState } from 'react';
 import './App.css'
 
 
 
 function App() {
+  const [isOnAddMode, setIsOnAddMode] = useState(false);
+
+  const mostraSkill = () => {
+    setIsOnAddMode(!isOnAddMode);
+  };
+
   return (
     <div className="App">
       <h2>Skills</h2>
-      <div className="flex-container">
-        <div className='skill-item'>
+      <button className="botaoAzul" onClick={mostraSkill}>
+        {isOnAddMode ? 'Fechar' : 'Adicionar'}
+      </button>
+      {isOnAddMode && (
+        <div className="flex-container skillsSelect">
+          <div className='skill-item'>
+            <h3>Hard Skills</h3>
+            <Multiselect /> {/* Certifique-se de importar o componente Multiselect corretamente */}
+          </div>
+          <div className='skill-item'>
+            <h3>Soft Skills</h3>
+            <Multiselect /> {/* Certifique-se de importar o componente Multiselect corretamente */}
+          </div>
+        </div>
+      )}
+      
+      <div className='flex-container'>
+      <div className='skill-item'>
           <h3>Hard Skills</h3>
-        <Multiselect/>
+            <div className="show-skills">
+              <p>mostra</p>
+            </div>
         </div>
         <div className='skill-item'>
           <h3>Soft Skills</h3>
-        <Multiselect/>
+          <div className="show-skills">
+              <p>mostra</p>
+          </div>
         </div>
-      </div>  
+      </div> 
       <Habilidade/>
       <Formacao/>
       
